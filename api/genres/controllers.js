e2ae6c18aacc5ddaea59e918d4af81f636efbffb
Ketunze Brain <brainketunze@gmail.com>
2021-03-26 14:48:56 +0100

    api update files

--- api/genres/controllers.js
+++ api/genres/controllers.js
@@ -79,6 +79,40 @@ const controllers = {
   },
   update: (req, res) => {
     // read row data from body
+    const re = req.body;
+    //first check if that item exists
+    let sql = `SELECT * FROM genres WHERE genreId =  ${req.params.id}`;
+
+    db.all(sql, (err, rows) => {
+      if (err) {
+        res.status(400).json({
+          "error": err.message
+        });
+        return;
+      }
+      if (rows.length === 0) return res.json({
+        "error": "no data found"
+      });
+
+      else {
+
+        //there's an item with that id, it's okay to modify it
+        sql = `UPDATE genres SET Name="${re.name}" WHERE genreId = ${req.params.id}`;
+
+        db.all(sql, (err, rows) => {
+          if (err) {
+            res.status(400).json({
+              "error": err.message
+            });
+            return;
+          }
+          res.json({
+            "message": "genre changed"
+          });
+        });
+
+      }
+    });
   },
   delete: (req, res) => {
 
@@ -109,7 +143,7 @@ const controllers = {
             return;
           }
           res.json({
-            "message": "genres deleted"
+            "message": "genre deleted"
           });
         });
 
